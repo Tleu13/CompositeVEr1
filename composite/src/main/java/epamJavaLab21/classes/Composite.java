b47a762912b3ed8cf4439c8ce3bf93e526ddc817
Tleu13 <tleuzhan13@gmail.com>
2017-02-11 11:36:13 +0600

    Add files via upload

---
+++ composite/src/main/java/epamJavaLab21/classes/Composite.java
@@ -0,0 +1,30 @@
+package epamJavaLab21.classes;
+
+import epamJavaLab21.interfaces.Printable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by TleuAkma on 09.02.2017.
+ */
+public class Composite implements Printable {
+
+    private List<Printable> childPrintable = new ArrayList<>();
+
+    public void add(Printable printable) {
+        childPrintable.add(printable);
+    }
+
+    public void remove(Printable printable) {
+        childPrintable.remove(printable);
+    }
+
+
+    @Override
+    public void print() {
+        for (Printable printable: childPrintable) {
+            printable.print();
+        }
+    }
+}
